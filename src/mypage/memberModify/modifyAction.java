b344c4bc14e55509a8edeb23a4d32534fcc1fd97
명준 <mjkim0357@gmail.com>
2017-11-15 09:29:04 +0900

    15일 첫병합

--- src/mypage/memberModify/modifyAction.java
+++ src/mypage/memberModify/modifyAction.java
@@ -33,20 +33,20 @@ public class modifyAction extends ActionSupport{
 	
 	private Map session;
 	
-	//생성자
+	//�깮�꽦�옄
 	public modifyAction() throws IOException{
-		reader = Resources.getResourceAsReader("sqlMapConfig.xml"); // sqlMapConfig.xml 파일의 설정내용을 가져온다.
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml"); // sqlMapConfig.xml �뙆�씪�쓽 �꽕�젙�궡�슜�쓣 媛��졇�삩�떎.
 		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
 		reader.close();
 	}
 	
-	// 회원정보 수정
+	// �쉶�썝�젙蹂� �닔�젙
 	public String execute() throws Exception{
-		// 파라미터와 리절트 객체 생성
+		// �뙆�씪誘명꽣�� 由ъ젅�듃 媛앹껜 �깮�꽦
 		paramClass = new memberBean();
 		resultClass = new memberBean();
 		
-		//수정할 항목 설정
+		//�닔�젙�븷 �빆紐� �꽕�젙
 		paramClass.setMember_no(member_no);
 		paramClass.setMember_name(member_name);
 		paramClass.setMember_email(member_email);
@@ -56,8 +56,8 @@ public class modifyAction extends ActionSupport{
 		paramClass.setMember_phone(member_phone);
 		paramClass.setMember_pw(member_pw);
 		
-		// 일단 항목만 수정한다.
+		// �씪�떒 �빆紐⑸쭔 �닔�젙�븳�떎.
 		sqlMapper.update("updateMember",paramClass);
-		
+		return SUCCESS;
 	}
 }
