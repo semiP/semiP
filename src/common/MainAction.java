8deef63ff7efa5e6e07c08a4992c9c72fad2105e
Jason Park <cayori@gmail.com>
2017-11-21 17:20:43 +0900

    login, sessionAware 체크

--- src/common/MainAction.java
+++ src/common/MainAction.java
@@ -8,6 +8,7 @@ import common.hitVO;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.LinkedHashMap;
 
 import com.ibatis.common.resources.Resources;
@@ -17,9 +18,11 @@ import com.ibatis.sqlmap.client.SqlMapClientBuilder;
 import java.io.Reader;
 import java.io.IOException;
 
+import org.apache.struts2.interceptor.SessionAware;
 
 
-public class MainAction extends ActionSupport {
+
+public class MainAction extends ActionSupport implements SessionAware {
 
 	private static final long serialVersionUID = 1L;
 
@@ -29,6 +32,9 @@ public class MainAction extends ActionSupport {
 	private List<hitVO> listHit = new ArrayList<hitVO>();	
 	private LinkedHashMap<String, List<goodsVO>> categoryMap = new LinkedHashMap<String, List<goodsVO>>();
 	
+	private Map session;
+	
+	
 	public MainAction() throws IOException{
 		reader = Resources.getResourceAsReader("sqlMapConfig.xml"); 
 		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader); 
@@ -37,6 +43,10 @@ public class MainAction extends ActionSupport {
 	
 	public String execute() throws Exception {
 		// hit 테이블 전부를 가져와서 List 로 만든다. hit 리스트는 7줄로 정해져 있다.
+		if(session.get("session_member_id") == null){
+			return LOGIN;
+		}
+		
 		listHit = sqlMapper.queryForList("mainSearch.select-hit-list");
 				
 		for(int i=0; i<listHit.size(); i++) {				// 매 히트 카테고리마다 반복한다.
@@ -68,6 +78,14 @@ public class MainAction extends ActionSupport {
 
 	public void setCategoryMap(LinkedHashMap<String, List<goodsVO>> categoryMap) {
 		this.categoryMap = categoryMap;
+	}
+	
+	public Map getSession() {
+		return session;
+	}
+
+	public void setSession(Map session) {
+		this.session = session;
 	}
 
 
