d6f805f7d7863f1e4b6cadb95303be4168366be1
JP <cayori@gmail.com>
2017-11-22 01:56:23 +0900

    goodsViewAction 에서 이미지 띄우는 것까지 작업... 하다보니까 부분을 나눠서 작업하기가 애매하네...

--- src/goods/goodsViewAction.java
+++ src/goods/goodsViewAction.java
@@ -10,24 +10,26 @@ import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
 
 import java.io.Reader;
-import java.io.InputStream;
 import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class goodsViewAction extends ActionSupport{
+import org.apache.struts2.interceptor.SessionAware; 
+
+public class goodsViewAction extends ActionSupport implements SessionAware {
 	
 	private static final long serialVersionUID = 1L;
 	
 	public static Reader reader;
 	public static SqlMapClient sqlMapper;
 	
-	
 	private goodsVO resultGoods = new goodsVO();
 	private List<goodsCmtVO> listCmt = new ArrayList<goodsCmtVO>();
 		
 	private int goods_no;
+	private Map session;
 
 	
 	public goodsViewAction() throws IOException
@@ -41,54 +43,41 @@ public class goodsViewAction extends ActionSupport{
 	public String execute() throws Exception
 	{
 		resultGoods = (goodsVO) sqlMapper.queryForObject("goods.select-goods-one", goods_no);
-		listCmt = 
+		listCmt = sqlMapper.queryForList("goods.select-cmt-all", goods_no);
 		
 		return SUCCESS;
 	}
 
-	public int getGoods_no() { return goods_no; }
-	public void setGoods_no (int goods_no) { this.goods_no = goods_no; }
-	
-	public goodsVO getParamClass() { return paramClass; }
-	public void setParamClass(goodsVO paramClass) { this.paramClass = paramClass; }
-	
-	public goodsVO getResultClass() { return resultClass; }
-	public void setResultClass(goodsVO resultClass) { this.resultClass = resultClass; }
-	
-	public String getFileUploadPath() { return fileUploadPath; }
-	public void setFileUploadPath(String fileUploadPath) { this.fileUploadPath = fileUploadPath; }
 
-	public InputStream getInputStream() { return inputStream; }
-	public void setInputStream(InputStream inputStream) { this.inputStream = inputStream; }
-	
-	public String getContentDisposition() { return contentDisposition; }
-	public void setContentDisposition(String contentDisposition) { this.contentDisposition = contentDisposition; }
-	
-	public long getContentLength() { return contentLength; }
-	public void setContentLength(long contentLength) { this.contentLength = contentLength; }
-	
-	public List<goodsCmtVO> getList() { return list; }
-	public void setList(List<goodsCmtVO> list) { this.list = list; }
-	
-	public int getCurrentPage() { return currentPage; }
-	public void setCurrentPage(int currentPage) { this.currentPage = currentPage; }
+	public goodsVO getResultGoods() {
+		return resultGoods;
+	}
 
-	public int getTotalCount() { return totalCount; }
-	public void setTotalCount(int totalCount) { this.totalCount = totalCount; }
+	public void setResultGoods(goodsVO resultGoods) {
+		this.resultGoods = resultGoods;
+	}
 
-	public int getBlockCount() { return blockCount; }
-	public void setBlockCount(int blockCount) { this.blockCount = blockCount; }
+	public List<goodsCmtVO> getListCmt() {
+		return listCmt;
+	}
 
-	public int getBlockPage() { return blockPage; }
-	public void setBlockPage(int blockPage) { this.blockPage = blockPage; }
+	public void setListCmt(List<goodsCmtVO> listCmt) {
+		this.listCmt = listCmt;
+	}
 
-	public String getPagingHtml() { return pagingHtml; }
-	public void setPagingHtml(String pagingHtml) { this.pagingHtml = pagingHtml; }
-	
-	public int getGoods_cmt_no() { return goods_cmt_no; }
-	public void setGoods_cmt_no(int goods_cmt_no) { this.goods_cmt_no = goods_cmt_no; }
-	
-	public String getGoods_cmt_pw() { return goods_cmt_pw; }
-	public void setGoods_cmt_pw(String goods_cmt_pw) { this.goods_cmt_pw = goods_cmt_pw; }
+	public int getGoods_no() {
+		return goods_no;
+	}
 
+	public void setGoods_no(int goods_no) {
+		this.goods_no = goods_no;
+	}
+
+	public Map getSession() {
+		return session;
+	}
+
+	public void setSession(Map session) {
+		this.session = session;
+	}
 }
